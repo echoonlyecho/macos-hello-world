e4009ea944ba06901a8dd741e5842fba10be155d
huirui <huirui@huanqiu.com>
2025-12-17 11:21:24 +0800

    Initial Commit

---
+++ hello-world/hello_worldApp.swift
@@ -0,0 +1,17 @@
+//
+//  hello_worldApp.swift
+//  hello-world
+//
+//  Created by huanqiu.com on 2025/12/17.
+//
+
+import SwiftUI
+
+@main
+struct hello_worldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
